e51605eae34feadf71f3940a299a88871e42637d
Anjar1912 <61839743+Anjar1912@users.noreply.github.com>
2020-06-11 15:07:25 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Abstraction.AbstractClass;
+//using Abstraction.Interface;
+
+namespace Abstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kendaraan kendaraan;
+            kendaraan = new Drone();
+            kendaraan.Bergerak();
+
+            Console.WriteLine();
+            kendaraan = new Motor();
+            kendaraan.Bergerak();
+
+            Console.WriteLine();
+            kendaraan = new Kapal();
+            kendaraan.Bergerak();
+
+
+            /*Kendaraan kendaraan;
+            kendaraan = new Drone();
+            kendaraan.Bergerak();
+
+            Console.WriteLine();
+            kendaraan = new Motor();
+            kendaraan.Bergerak();
+
+            Console.WriteLine();
+            kendaraan = new Kapal();
+            kendaraan.Bergerak()*/
+
+            Console.ReadKey();
+        }
+    }
+}
